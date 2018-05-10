0f55877275a7e660e3eb8faa5f2c15720d0dcd01
Shumai <shumai9@github.com>
2018-05-10 19:08:26 +0100

    removed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
-class UsersController < ApplicationController
+# class UsersController < ApplicationController
 
-def index
-  @user = current_user
-  @rooms = Room.all.sorted
-end
+# def index
+#   @user = current_user
+#   @rooms = Room.all.sorted
+# end
 
- def show
-  @user = User.find(params[:id])
-  @rooms = Room.all
- end
+#  def show
+#   @user = User.find(params[:id])
+#   @rooms = Room.all
+#  end
 
- end
+# end
+#  
